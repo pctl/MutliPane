cdce1fbbbea3a5fa76a3d3499462f6726dab9634
Georgia <quatregeorgia@gmail.com>
2015-11-22 23:35:36 +0300

    Initial commit

---
+++ app/src/main/java/com/georgia/bunihubtrial/codelab1/mutlipane/SidePanelFragment.java
@@ -0,0 +1,21 @@
+package com.georgia.bunihubtrial.codelab1.mutlipane;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Georgia on 11/22/2015.
+ */
+public class SidePanelFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       // return super.onCreateView(inflater, container, savedInstanceState);
+        View view=inflater.inflate(R.layout.fragment_side_panel,container,false);
+        return view;
+    }
+}
